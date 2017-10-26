c7d31396bf46e63c703f87ce33c119d320f371be
florianthalmann <florianthalmann@bluewin.ch>
2017-10-26 15:31:05 +0100

    worked on types

--- src/types.ts
+++ src/types.ts
@@ -6,42 +6,63 @@ export interface SchedulerEvent {}
 export interface AudioEvent extends SchedulerEvent {
 	fileUris: string[]
 }
-export interface AudioEvent extends SchedulerEvent {
-	fileUris: string[]
+export interface TriggerEvent extends SchedulerEvent {
+  trigger: Function
+}
+
+export interface Time {
+	time: number;
+}
+export interface Beat {
+	beat: number;
+}
+export interface Bar {
+  bar: number;
 }
 
 export interface SchedulingTime {}
-export interface Immediately extends SchedulingTime {}
-export interface Time extends SchedulingTime {
-	time: number;
+export interface StartImmediately extends SchedulingTime {}
+export interface StartAt extends SchedulingTime {
+  time: Time | Beat | Bar
 }
-export interface Beat extends SchedulingTime {
-	beat: number;
-}
-export interface Bar extends SchedulingTime {}
-export interface NextBeat extends SchedulingTime {}
-export interface NextBar extends SchedulingTime {}
-
-export interface SchedulingMode {}
-export interface Oneshot extends SchedulingMode {}
-export interface Loop extends SchedulingMode {}
-export interface Repeat extends SchedulingMode {
-  times: number
+export interface StartNext extends SchedulingTime {
+  time: Beat | Bar
 }
 
-export interface SchedulingOptions {
-  mode: SchedulingMode
+export interface PlaybackMode {}
+export interface Oneshot extends PlaybackMode {}
+export interface Loop extends PlaybackMode {
+  times?: number
+}
+
+export interface TransitionMode {}
+export interface TransitionImmediately extends TransitionMode {}
+export interface Crossfade extends TransitionMode {
+  duration: Time | Beat | Bar
+}
+
+export interface StoppingMode {}
+export interface Immediate extends StoppingMode {}
+export interface Fade extends StoppingMode {
+  duration: Time | Beat | Bar
 }
 
 export interface Scheduler {
-  schedule(event: SchedulerEvent, time: SchedulingTime, mode: SchedulingMode): SchedulerId;
-  schedulerAfter(id: SchedulerId, event: SchedulerEvent, mode: SchedulingMode): SchedulerId;
 
-  stop(id: SchedulerId, time: SchedulingTime): SchedulerId;
+  start(event: SchedulerEvent, time: SchedulingTime, mode: PlaybackMode): SchedulerId;
+  startAfter(id: SchedulerId, event: SchedulerEvent, mode: PlaybackMode): SchedulerId;
+
+  transitionTo(event: SchedulerEvent, time: SchedulingTime, transitionMode: TransitionMode, playbackMode: PlaybackMode): SchedulerId;
+
+  replace(id: SchedulerId, time: SchedulingTime, mode: PlaybackMode): SchedulerId;
+
+  stop(id: SchedulerId, time: SchedulingTime, mode: StoppingMode): void;
+  stopAll(time: SchedulingTime, mode: StoppingMode): void;
+
+  watch(id: SchedulerId): Observable<SchedulerEvent>; //TODO change
 
   getCurrentTime(): number;
   getCurrentBeat(): number;
   getCurrentBar(): number;
 
-  watch(id: SchedulerId): Observable<SchedulerEvent>; //TODO change
 }
