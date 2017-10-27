a0c50d0a3f293a1dc84d23af318e9d232cbe60f4
Lucas Thompson <dev@lucas.im>
2017-10-27 13:25:36 +0100

    Add type definitions for Time and TimeBase

--- src/Tone.d.ts
+++ src/Tone.d.ts
@@ -1,7 +1,46 @@
-declare const ToneInstance: Tone;
+type UntypedBodge = {[key: string]: any}
+declare const ToneInstance: Tone & UntypedBodge;
 
 declare module 'tone' {
   export = ToneInstance;
 }
 
-type Tone = any;
+type Tone = {
+  Time: TimeConstructor;
+  TimeBase: TimeBaseConstructor;
+};
+
+type BarsBeatsSixteenths = string;
+type Notation = string;
+
+interface TimeBase {
+  add(val: Time, units?: string): TimeBase;
+  clone(): TimeBase;
+  dispose(): void;
+  div(val: Time, units?: string): TimeBase;
+  mult(val: Time, units?: string): TimeBase;
+  set(exprString: string): TimeBase;
+  sub(val: Time, units?: string): TimeBase;
+  valueOf(): number;
+}
+
+interface Time extends TimeBase {
+  addNow(): Time;
+  copy(): Time;
+  quantize(): Time;
+  toBarsBeatsSixteeths(): BarsBeatsSixteenths;
+  toFrequency(): number;
+  toMilliseconds(): number;
+  toNotation(): Notation;
+  toSamples(): number;
+  toSeconds(): number;
+  toTicks(): number;
+}
+
+interface TimeBaseConstructor {
+  new(val: string | number, units?: string): TimeBase;
+}
+
+interface TimeConstructor {
+  new(val: string | number, units?: string): Time;
+}
