36bcd7e25d440a942615b7c85e7d8ec4fea06b43
Lucas Thompson <dev@lucas.im>
2017-11-22 14:14:59 +0000

    Small refactoring / tidy of buffer loading and play instantiation

--- src/tone-helpers.ts
+++ src/tone-helpers.ts
@@ -35,56 +35,63 @@ function sub(t1: string | number, t2: string | number): number {
   return new Time(t1).sub(new Time(t2)).toSeconds();
 }
 
+export function createTonePlayerWithBuffer(
+  scheduleOpts: ScheduleOptions,
+  playerOpts: SubsetPlayerOptions,
+  buffer: ToneBuffer
+): PlayerFactory {
+  const {startTime, offset = 0} = scheduleOpts;
+  const {url, ...otherOpts} = playerOpts;
+
+  const calculateDuration = (
+    buffer: AudioBuffer,
+    {duration} = scheduleOpts
+  ): number => {
+    return duration ?
+      new Time(duration).toSeconds() :
+      buffer.duration - new Time(offset).toSeconds();
+  };
+
+  const setLoopPoints = (player: Player, duration: number) => {
+    if (playerOpts.loop) {
+      player.loopStart = new Time(offset).toSeconds();
+      player.loopEnd = add(duration, offset);
+    }
+  }
+
+  const createAndInitPlayer = (buffer: ToneBuffer) => {
+    const player = new Player(buffer);
+    const {loop = false, playbackRate = 1} = otherOpts;
+    player.loop = loop;
+    player.playbackRate = playbackRate;
+    const duration = calculateDuration(buffer.get());
+    setLoopPoints(player, duration)
+    return player;
+  };
+
+  const createPlayer = () => createAndInitPlayer(buffer);
+  const duration = calculateDuration(buffer.get());
+  return {
+    createPlayer,
+    options: {startTime, offset, duration},
+    buffer: buffer.get()
+  };
+}
+
 export function createTonePlayer(
   scheduleOpts: ScheduleOptions,
   playerOpts: SubsetPlayerOptions,
   filenameCache: Map<String, AudioBuffer>
 ): Promise<PlayerFactory> {
+  const { url } = playerOpts;
   return new Promise((resolve, reject) => {
-    const {startTime, offset = 0} = scheduleOpts;
-    const {url, onload = () => {}, ...otherOpts} = playerOpts;
-
-    const calculateDuration = (
-      buffer: AudioBuffer,
-      {duration} = scheduleOpts
-    ): number => {
-      return duration ?
-        new Time(duration).toSeconds() :
-        buffer.duration - new Time(offset).toSeconds();
-    };
-
-    const setLoopPoints = (player: Player, duration: number) => {
-      if (playerOpts.loop) {
-        player.loopStart = new Time(offset).toSeconds();
-        player.loopEnd = add(duration, offset);
-      }
-    }
-
-    const createAndInitPlayer = (buffer: ToneBuffer) => {
-      const player = new Player(buffer);
-      const {loop = false, playbackRate = 1} = otherOpts;
-      player.loop = loop;
-      player.playbackRate = playbackRate;
-      const duration = calculateDuration(buffer.get());
-      setLoopPoints(player, duration)
-      return player;
-    };
-
     if (filenameCache.has(url)) {
       const buffer = new Tone.Buffer(
         filenameCache.get(url),
         (loaded: ToneBuffer) => {
-          const createPlayer = () => createAndInitPlayer(loaded);
-          // if we already have the buffer, manually resolve
-          // (Tone.js doesn't call onload for Buffers)
-          const buffer = loaded.get();
-          const duration = calculateDuration(buffer); // TODO remove dupe
-          resolve({
-            createPlayer,
-            options: {startTime, offset, duration},
-            buffer
-          });
-        }
+          resolve(createTonePlayerWithBuffer(scheduleOpts, playerOpts, loaded));
+        },
+        (err: string) => reject(err)
       );
     } else {
       const buffer = new Tone.Buffer(
@@ -92,13 +99,7 @@ export function createTonePlayer(
         (loaded: ToneBuffer) => {
           const buffer = loaded.get();
           filenameCache.set(url, buffer);
-          const duration = calculateDuration(buffer);
-          const createPlayer = () => createAndInitPlayer(loaded);
-          resolve({
-            createPlayer,
-            options: {startTime, offset, duration},
-            buffer
-          });
+          resolve(createTonePlayerWithBuffer(scheduleOpts, playerOpts, loaded));
         },
         (err: string) => reject(err)
       );
